517783aafa91914cd8701801a9df6a31f02761ba
fendouluo <422902732@qq.com>
2019-10-24 05:04:59 -0700

    add some demo

---
+++ avg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+N = int(input("Enter your Digital number :"))
+
+while N <= 0:
+	N = int(input("Enter your Digital number :"))
+a = 1
+value = 0
+avg = 0
+while a <= N:
+	tmp = float(input('Enter your the {} nuber: '.format(a)))
+	value = value + tmp
+	a = a + 1
+avg = value/N
+#print(avg)
+print('avg is :{}'.format(avg))
+	
---
+++ helloworld.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import math
+
+print('Hello World!')
+print(math.e)
+
