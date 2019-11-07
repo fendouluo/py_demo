8796af5c480b6204c27f3fbb3285d2670a21e11b
fendouluo <422902732@qq.com>
2019-11-06 22:55:58 -0800

    word count

---
+++ countwords.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+s = input("Enter a line: ")
+print("The number of words in the line are %d " % (len(s.split(" "))))
+
+#%s 字符串（用 str() 函数进行字符串转换）
+#%r 字符串（用 repr() 函数进行字符串转换）
+#%d 十进制整数
+#%f 浮点数
+#%% 字符“%”
