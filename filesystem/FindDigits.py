9f3605e0346b45ec598c3b526d95be7500f24241
fendouluo <422902732@qq.com>
2019-11-09 00:39:53 -0800

    Extract the number in the string in the file

--- filesystem/FindDigits.py
+++ filesystem/FindDigits.py
@@ -4,5 +4,25 @@ import os
 import sys
 
 def finddigits(path):
-	with open(path) as f:
-	s = f.read()
+	res = ""
+	with open(path) as file:
+		try:
+			while True:
+				text_line = file.readline()
+				if text_line:
+					print(type(text_line), text_line)
+					for char in text_line:
+						if char.isdigit():
+							res += char
+				else:
+					break
+		finally:
+			file.close()
+	print(res)
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		finddigits(sys.argv[1])
+	else:
+		sys.exit(-1)
+	sys.exit(0)
