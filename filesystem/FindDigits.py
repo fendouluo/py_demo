10d16e7d58e31534244c092f6b58afdb8c7bb1ca
fendouluo <422902732@qq.com>
2019-11-08 22:44:36 -0800

    Add some files to read demo

---
+++ filesystem/FindDigits.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def finddigits(path):
+	with open(path) as f:
+	s = f.read()
--- filesystem/fileread.py
+++ filesystem/fileread.py
@@ -6,6 +6,40 @@ print("""\
 ... fobj = open('sample.txt')
 ... for x in fobj:
 ...     print(x, end = '')
+... 在实际情况中，我们应该尝试使用 with 语句处理文件对象，它会在文件用完后会自动关闭，就算发生异常也没关系。它是 try-finally 块的简写：
+... with open('sample.txt') as fobj:
+...     for line in fobj:
+...         print(line, end = '')
+... file = open('兼职模特联系方式.txt', 'r')  # 创建的这个文件，也是一个可迭代对象
+try:
+    text = file.read()  # 结果为str类型
+    print(type(text))
+    print(text)
+finally:
+    file.close()
+
+file = open('兼职模特联系方式.txt', 'r')
+
+try:
+    while True:
+        text_line = file.readline()
+        if text_line:
+            print(type(text_line), text_line)
+        else:
+            break
+finally:
+    file.close()
+
+file = open('兼职模特联系方式.txt', 'r')
+
+try:
+    text_lines = file.readlines()
+    print(type(text_lines), text_lines)
+    for line in text_lines:
+        print(type(line), line)
+finally:
+    file.close()
+
 ... """)
 name = input("Enter the file name:")
 fobj = open(name)
