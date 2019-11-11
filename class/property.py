c53264b4107ea2914fb0f4815222ab797c3d9786
fendouluo <422902732@qq.com>
2019-11-11 01:12:13 -0800

    property (class decorator)

---
+++ class/property.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+print("""
+	你可能想要更精确的调整控制属性访问权限，你可以使用 @property 装饰器，@property 装饰器就是负责把一个方法变成属性调用的。
+	""")
+
+class Account(object):
+    """账号类,
+    amount 是美元金额.
+    """
+    def __init__(self, rate):
+        self.__amt = 0
+        self.rate = rate
+
+    @property
+    def amount(self):
+        """账号余额（美元）"""
+        return self.__amt
+
+    @property
+    def cny(self):
+        """账号余额（人民币）"""
+        return self.__amt * self.rate
+
+    @amount.setter
+    def amount(self, value):
+        if value < 0:
+            print("Sorry, no negative amount in the account.")
+            return
+        self.__amt = value
+
+if __name__ == '__main__':
+    acc = Account(rate=7.0) # 基于课程编写时的汇率
+    acc.amount = 20
+    print("Dollar amount:", acc.amount)
+    print("In CNY:", acc.cny)
+    acc.amount = -100
+    print("Dollar amount:", acc.amount)
